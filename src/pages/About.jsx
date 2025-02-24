5e7e832e54b4f841e9a335660b0cc78992653231
AlberteMB <albertebardanca@hotmail.com>
2025-02-24 19:29:37 +0100

    Solving issues in design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import '../style/App.css';
 const About = () => {
     
   return (
-<p className="about-description">
+<p className="description">
   Thank you for using Weather Tracker! If you have any questions or suggestions, feel free to <a href="mailto:support@weathertracker.com" className="about-link">contact us</a>.
 </p>    
   );
